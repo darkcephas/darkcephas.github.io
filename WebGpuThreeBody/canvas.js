2b955d72288c9fb6bee889e2595f4feb6930589d
peter mcneeley <petermcneeley@hotmail.com>
2025-06-08 17:22:30 -0400

    Init

---
+++ WebGpuThreeBody/canvas.js
@@ -0,0 +1,211 @@
+var device;
+var canvasformat;
+var context;
+const NUM_PARTICLES_DIM = 256;
+var canvas_width;
+var canvas_height;
+var bindGroupLayout;
+var uniformBuffer;
+var simulationBindGroups;
+var massAssignBindGroups;
+var starGraphicsBindGroup;
+var massGraphicsBindGroup;
+var forceIndexBindGroups;
+
+window.onload =  async  function () {
+  const canvas = document.querySelector("canvas");
+  if (!canvas) {
+    throw new Error("No canvas.");
+  }
+  canvas_width = canvas.width;
+  canvas_height = canvas.height;
+  // Your WebGPU code will begin here!
+  if (!navigator.gpu) {
+    throw new Error("WebGPU not supported on this browser.");
+  }
+  const adapter = await navigator.gpu.requestAdapter();
+    if (!adapter) {
+      throw new Error("No appropriate GPUAdapter found.");
+    }
+    device = await adapter.requestDevice();
+    context = canvas.getContext("webgpu");
+    canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+    context.configure({
+      device: device,
+      format: canvasFormat,
+    });
+    
+
+    
+    // Create the bind group layout and pipeline layout.
+     bindGroupLayout = device.createBindGroupLayout({
+      label: "Cell Bind Group Layout",
+      entries: [{
+        binding: 0,
+        visibility: GPUShaderStage.VERTEX | GPUShaderStage.COMPUTE |  GPUShaderStage.FRAGMENT,
+        buffer: {} // Grid uniform buffer
+      }, {
+        binding: 1,
+        visibility: GPUShaderStage.VERTEX | GPUShaderStage.COMPUTE |  GPUShaderStage.FRAGMENT,
+        buffer: { type: "read-only-storage"} // Cell state input buffer
+      }, {
+        binding: 2,
+        visibility: GPUShaderStage.COMPUTE,
+        buffer: { type: "storage"} // Cell state output buffer
+      }]
+    });
+    
+    const pipelineLayout = device.createPipelineLayout({
+      label: "Cell Pipeline Layout",
+      bindGroupLayouts: [ bindGroupLayout ],
+    });
+    
+   setup_render_particles(pipelineLayout);
+    setup_compute_particles(pipelineLayout);
+ 
+    
+    // Create a uniform buffer that describes the grid.
+    const uniformArray = new Float32Array([canvas_width, canvas_height]);
+    uniformBuffer = device.createBuffer({
+      label: "Grid Uniforms",
+      size: uniformArray.byteLength,
+      usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+    });
+    device.queue.writeBuffer(uniformBuffer, 0, uniformArray);
+    // Create an array representing the active state of each cell.
+ 
+
+    commonBindGroup = [
+      device.createBindGroup({
+        label: "Compute renderer bind group A",
+        layout: bindGroupLayout, // Updated Line
+        entries: [{
+          binding: 0,
+          resource: { buffer: uniformBuffer }
+        }, {
+          binding: 1,
+          resource: { buffer: cellStateStorage }
+        }, {
+          binding: 2, // New Entry
+          resource: { buffer: renderBufferStorage }
+        }],
+      }),
+      device.createBindGroup({
+        label: "Compute renderer bind group B",
+        layout: bindGroupLayout, // Updated Line
+
+        entries: [{
+          binding: 0,
+          resource: { buffer: uniformBuffer }
+        }, {
+          binding: 1,
+          resource: { buffer: cellStateStorage }
+        }, {
+          binding: 2, // New Entry
+          resource: { buffer: renderBufferStorage }
+        }],
+      }),
+    ];
+ 
+
+    massGraphicsBindGroup = 
+      device.createBindGroup({
+        label: "Compute renderer bind group A",
+        layout: bindGroupLayout, // Updated Line
+        entries: [{
+          binding: 0,
+          resource: { buffer: uniformBuffer }
+        }, {
+          binding: 1,
+          resource: { buffer: massAssignBufferStorage }
+        }, {
+          binding: 2, // New Entry
+          resource: { buffer:  cellStateStorage}
+        }],
+      });
+ 
+    starGraphicsBindGroup = 
+      device.createBindGroup({
+        label: "Compute renderer bind group A",
+        layout: bindGroupLayout, // Updated Line
+        entries: [{
+          binding: 0,
+          resource: { buffer: uniformBuffer }
+        }, {
+          binding: 1,
+          resource: { buffer: renderBufferStorage }
+        }, {
+          binding: 2, // New Entry
+          resource: { buffer:  cellStateStorage}
+        }],
+      });
+ 
+
+    simulationBindGroups =  
+      device.createBindGroup({
+        label: "Cell renderer bind group A",
+        layout: bindGroupLayout, // Updated Line
+        entries: [{
+          binding: 0,
+          resource: { buffer: uniformBuffer }
+        }, {
+          binding: 1,
+          resource: { buffer: forceIndexBufferStorage }
+        }, {
+          binding: 2, // New Entry
+          resource: { buffer: cellStateStorage }
+        }],
+      });
+
+      massAssignBindGroups =  
+      device.createBindGroup({
+        label: "mass assign",
+        layout: bindGroupLayout, // Updated Line
+        entries: [{
+          binding: 0,
+          resource: { buffer: uniformBuffer }
+        }, {
+          binding: 1,
+          resource: { buffer: cellStateStorage }
+        }, {
+          binding: 2, // New Entry
+          resource: { buffer: massAssignBufferStorage }
+        }],
+      });
+
+      forceIndexBindGroups =  
+      device.createBindGroup({
+        label: "Force Index",
+        layout: bindGroupLayout, // Updated Line
+        entries: [{
+          binding: 0,
+          resource: { buffer: uniformBuffer }
+        }, {
+          binding: 1,
+          resource: { buffer: massAssignBufferStorage }
+        }, {
+          binding: 2, // New Entry
+          resource: { buffer: forceIndexBufferStorage }
+        }],
+      });
+
+
+
+    const UPDATE_INTERVAL = 1; // 
+    let step = 0; // Track how many simulation steps have been run
+        
+    function updateGrid() {
+      step++; // Increment the step count
+      
+      // Start a render pass 
+      const encoder = device.createCommandEncoder();
+      update_compute_particles(encoder, commonBindGroup, step);
+      draw_particles(encoder, step);
+      const commandBuffer = encoder.finish();
+      device.queue.submit([commandBuffer]);
+      window.requestAnimationFrame(updateGrid);
+    }
+
+    //window.setInterval(updateGrid, UPDATE_INTERVAL);
+    window.requestAnimationFrame(updateGrid);
+}
---
+++ WebGpuThreeBody/render_particles.js
@@ -0,0 +1,212 @@
+
+var cellPipeline;
+var vertexBuffer;
+var vertices;
+var massRenderPipeline;
+
+function setup_render_particles(pipelineLayout) {
+    vertices = new Float32Array([
+    //   X,    Y,
+      -1.0, -1.0, // Triangle 1 (Blue)
+       1.0, -1.0,
+       1.0,  1.0,
+
+      -1.0, -1.0, // Triangle 2 (Red)
+       1.0,  1.0,
+      -1.0,  1.0,
+    ]);
+    vertexBuffer = device.createBuffer({
+      label: "Cell vertices",
+      size: vertices.byteLength,
+      usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+    });
+    device.queue.writeBuffer(vertexBuffer, /*bufferOffset=*/0, vertices);
+    
+    const vertexBufferLayout = {
+      arrayStride: 8,
+      attributes: [{
+        format: "float32x2",
+        offset: 0,
+        shaderLocation: 0, // Position, see vertex shader
+      }],
+    };
+    
+    const cellShaderModule = device.createShaderModule({
+      label: 'Cell shader',
+      code: `
+        struct VertexInput {
+          @location(0) pos: vec2f,
+          @builtin(instance_index) instance: u32,
+        };
+
+        struct VertexOutput {
+          @builtin(position) pos: vec4f,
+          @location(0) vert_pos: vec2f, // Not used yet
+        };
+        
+        struct FragInput {
+          @location(0) vert_pos: vec2f,
+        };
+
+
+
+        @group(0) @binding(0) var<uniform> canvas_size: vec2f;
+        @group(0) @binding(1) var<storage> renderBufferIn: array<u32>;
+        @vertex
+        fn vertexMain(input: VertexInput) -> VertexOutput {
+          var output: VertexOutput;
+          let gridPos = input.pos.xy;
+          output.pos = vec4f(gridPos, 0, 1);
+          output.vert_pos = (input.pos.xy+1) *0.5* canvas_size; // New line!
+          return output;
+        }
+       @fragment
+        fn fragmentMain(input: FragInput) -> @location(0) vec4f {
+          let x_pixel = u32(input.vert_pos.x);
+          let y_pixel = u32(input.vert_pos.y);
+
+          let star_count = f32(renderBufferIn[x_pixel + u32(canvas_size.x)* y_pixel])/256.0;
+   
+          return vec4f(star_count/10.0,star_count/16.0,star_count/2.0 ,1) ;
+
+;
+        }
+      `
+    });
+    
+    cellPipeline = device.createRenderPipeline({
+      label: "Cell pipeline",
+      layout: pipelineLayout, // Updated!
+      vertex: {
+        module: cellShaderModule,
+        entryPoint: "vertexMain",
+        buffers: [vertexBufferLayout]
+      },
+      fragment: {
+        module: cellShaderModule,
+        entryPoint: "fragmentMain",
+        targets: [{
+          format: canvasFormat,
+          blend: {
+            alpha: {
+              dstFactor: "one",
+              srcFactor: "one",
+              operation: "add"
+            },
+            color: {
+              dstFactor: "one",
+              srcFactor: "one",
+              operation: "add"
+            },
+          },
+        }]
+      }
+    });
+
+
+    const massRenderShaderModule = device.createShaderModule({
+      label: 'Cell shader',
+      code: `
+        struct VertexInput {
+          @location(0) pos: vec2f,
+          @builtin(instance_index) instance: u32,
+        };
+
+        struct VertexOutput {
+          @builtin(position) pos: vec4f,
+          @location(0) vert_pos: vec2f, // Not used yet
+        };
+        
+        struct FragInput {
+          @location(0) vert_pos: vec2f,
+        };
+
+
+
+        @group(0) @binding(0) var<uniform> canvas_size: vec2f;
+        @group(0) @binding(1) var<storage> renderBufferIn: array<vec4u>;
+        @vertex
+        fn vertexMain(input: VertexInput) -> VertexOutput {
+          var output: VertexOutput;
+          let gridPos = input.pos.xy;
+          output.pos = vec4f(gridPos, 0, 1);
+          output.vert_pos = (input.pos.xy+1) *0.5* canvas_size; // New line!
+          return output;
+        }
+       @fragment
+        fn fragmentMain(input: FragInput) -> @location(0) vec4f {
+          let x_pixel = u32(input.vert_pos.x);
+          let y_pixel = u32(input.vert_pos.y);
+
+
+          let mass_assign_data = renderBufferIn[x_pixel/6+  (y_pixel/6)*128u];
+          var misaligned_amount = 0u;
+          if(mass_assign_data.x > 0)
+          {
+            misaligned_amount =(1+mass_assign_data.z-mass_assign_data.y)-mass_assign_data.x;
+          }
+          return vec4f(f32(misaligned_amount)/1.0, 0.0, 0.0 ,1.0);
+
+        }
+      `
+    });
+    
+    massRenderPipeline = device.createRenderPipeline({
+      label: "Cell pipeline",
+      layout: pipelineLayout, // Updated!
+      vertex: {
+        module: massRenderShaderModule,
+        entryPoint: "vertexMain",
+        buffers: [vertexBufferLayout]
+      },
+      fragment: {
+        module: massRenderShaderModule,
+        entryPoint: "fragmentMain",
+        targets: [{
+          format: canvasFormat,
+          blend: {
+            alpha: {
+              dstFactor: "one",
+              srcFactor: "one",
+              operation: "add"
+            },
+            color: {
+              dstFactor: "one",
+              srcFactor: "one",
+              operation: "add"
+            },
+          },
+        }]
+      }
+    });
+}
+
+function draw_particles(encoder, step)
+{
+       const pass = encoder.beginRenderPass({
+        colorAttachments: [{
+          view: context.getCurrentTexture().createView(),
+          loadOp: "clear",
+          clearValue: { r: 0, g: 0.0, b: 0.0, a: 1.0 },
+          storeOp: "store",
+        }]
+      });
+
+      // Draw the grid.
+      pass.setPipeline(cellPipeline);
+      pass.setBindGroup(0, starGraphicsBindGroup); // Updated!
+      pass.setVertexBuffer(0, vertexBuffer);
+      pass.draw(vertices.length / 2);
+      // End the render pass and submit the command buffer
+   
+
+      //Draw the grid.
+      pass.setPipeline(massRenderPipeline);
+      pass.setBindGroup(0, massGraphicsBindGroup); // Updated!
+      pass.setVertexBuffer(0, vertexBuffer);
+      pass.draw(vertices.length / 2);
+      // End the render pass and submit the command buffer
+      pass.end();
+}
+
+
